d28f0956cd4f3fdd978c5901063e94d83cca09de
송정 <tjsjttt@gmail.com>
2022-11-17 17:15:58 +0900

    vue 마크업 구성 완료와 스타일 적용 진행중

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,143 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+// console.log("AAAA");
 </script>
-
+<script>
+export default {
+  data() {
+    return {
+      brands: [
+        { id: 0, name: "Adidas" },
+        { id: 1, name: "Nike" },
+        { id: 2, name: "Fila" },
+        { id: 3, name: "PUMA" },
+        { id: 3, name: "SOME" },
+      ],
+    };
+  },
+};
+</script>
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <button
+        type="button"
+        class="comp-btn comp-btn__is-round comp-btn__is-only-icon nav-btn"
+      >
+        <span class="comp-btn__txt">menu</span>
+        <span class="comp-btn__icon"></span>
+      </button>
+      <RouterLink
+        to="/shop"
+        class="comp-btn comp-btn__is-round comp-btn__is-only-icon nav-btn"
+      >
+        <span class="comp-btn__txt">shop</span>
+        <span class="comp-btn__icon"></span
+      ></RouterLink>
+    </nav>
   </header>
 
-  <RouterView />
+  <main class="page-profilter">
+    <section>
+      <h2>Hemendra</h2>
+      <span class="is-font-color50 fs-15">Welcome to Stylish</span>
+      <form>
+        <div class="comp">
+          <input type="text" id="top_search" placeholder="Search..." />
+          <label for="top_search"></label>
+        </div>
+        <button
+          type="button"
+          class="comp-btn comp-btn__is-square comp-btn__is-only-icon comp-btn__is-large is-point-color__back"
+        >
+          <span class="comp-btn__txt">voice recognition</span>
+          <span class="comp-btn__icon"></span>
+        </button>
+      </form>
+    </section>
+
+    <section class="l-section">
+      <div class="l-section__header">
+        <h3>Choose Brand</h3>
+        <div class="l-section__header__right">
+          <button type="button">
+            <span class="is-font-color50 fs-13">View All</span>
+          </button>
+        </div>
+      </div>
+      <div class="l-section__content">
+        <div class="scroll-container filter-list">
+          <!-- for 문 돌릴 영역 -->
+          <div class="comp filter-list__item" v-for="brand in brands">
+            <input type="checkbox" id="bradList_00_01" />
+            <label
+              for="bradList_00_01"
+              class="comp-btn comp-btn__is-square comp-btn__is-add-icon"
+            >
+              <span class="comp-btn__icon"></span>
+              <span class="comp-btn__txt">{{ brand.name }}</span>
+            </label>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="l-section">
+      <div class="l-section__header">
+        <h3>New Arraival</h3>
+        <div class="l-section__header__right">
+          <button type="button">
+            <span class="is-font-color50 fs-13">View All</span>
+          </button>
+        </div>
+      </div>
+      <div class="l-section__content">
+        <!-- for 문 돌릴 영역 -->
+        <article class="card">
+          <div class="card__img">
+            <a href="">
+              <img src="" alt="" />
+            </a>
+            <button type="button" class="comp-btn comp-btn__is-only-icon">
+              <span class="comp-btn__icon"></span>
+              <span class="comp-btn__txt">favorite</span>
+            </button>
+          </div>
+          <div class="card__txtbox">
+            <a href="" class="">Nike Sportswear Club Fleece</a>
+            <strong class="">$99</strong>
+          </div>
+        </article>
+      </div>
+    </section>
+  </main>
+
+  <nav class="main-nav">
+    <ul>
+      <li>
+        <a href="#" class="comp-btn comp-btn__is-only-icon nav-btn">
+          <span class="comp-btn__txt">Home</span>
+          <span class="comp-btn__icon"></span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="comp-btn comp-btn__is-only-icon nav-btn">
+          <span class="comp-btn__txt">favorite</span>
+          <span class="comp-btn__icon"></span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="comp-btn comp-btn__is-only-icon nav-btn">
+          <span class="comp-btn__txt">shop</span>
+          <span class="comp-btn__icon"></span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="comp-btn comp-btn__is-only-icon nav-btn">
+          <span class="comp-btn__txt">notebook</span>
+          <span class="comp-btn__icon"></span>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
